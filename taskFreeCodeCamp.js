b9d51d864ef2479c201f4f366cbd4c9aa0d6fc09
Rodak-coder <allcity8888@gmail.com>
2021-10-26 21:24:11 +0300

    Update taskFreeCodeCamp.js

--- taskFreeCodeCamp.js
+++ taskFreeCodeCamp.js
@@ -23,3 +23,5 @@ var myAdjective = "big";
 var myVerb = "ran";
 var myAdverb = "quickly";
 var wordBlanks = myAdjective +' '+ myNoun +' '+  myVerb +' '+ myAdverb; 
+
+var myArray = ['I got', 5, 'on it'];

89d8b4d95d7198fe7789a0f95c9ab14007c5a22c
Rodak-coder <allcity8888@gmail.com>
2021-10-28 00:49:19 +0300

    Update tasks28.10.2021.js

--- tasks28.10.2021.js
+++ tasks28.10.2021.js
@@ -1,3 +1,8 @@
 function expressionMatter(a, b, c) {
     return Math.max(a*b*c,a*b+c,a+b*c,a+b+c,(a+b)*c,a*(b+c));
-  }
+  };
+  class SmallestIntegerFinder {
+    findSmallestInt(args) {
+    return Math.min(...args);
+    }
+  };

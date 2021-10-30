d331ee172e581a61058767a24fd822beb7432ab4
Rodak-coder <allcity8888@gmail.com>
2021-10-30 23:17:46 +0300

    Update tasks30.10.2021.js

--- tasks30.10.2021.js
+++ tasks30.10.2021.js
@@ -1,3 +1,10 @@
 function countSheeps(arrayOfSheep) {
     return arrayOfSheep.filter(sheep => sheep === true).length;
   }
+
+  function digital_root(n) {
+    if ( n < 10 ) return n;
+    return digital_root(n.toString().split('').reduce((a, b) => {
+        return a + +b;
+      },0));
+  }

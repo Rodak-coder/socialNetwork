3129a0d9a85505b6d1bd40fe2cbe47cc4839eec4
Rodak-coder <allcity8888@gmail.com>
2021-11-01 20:29:56 +0300

    Update tasks30.10.2021.js

--- tasks30.10.2021.js
+++ tasks30.10.2021.js
@@ -105,3 +105,11 @@ var greet_abe = function() {
     }, words[0]);
     return shortest.length;
   }
+
+  function domainName(url){
+    url = url.replace('http://', '');
+    url = url.replace('https://', '');
+    url = url.replace('www.', '');
+    
+    return url.split('.')[0];
+  }

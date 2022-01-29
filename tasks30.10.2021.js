f5b565e258fe91cbe68dd3665da2294eed7b748f
Rodak-coder <allcity8888@gmail.com>
2022-01-29 12:29:55 +0300

    Update tasks30.10.2021.js

--- tasks30.10.2021.js
+++ tasks30.10.2021.js
@@ -156,3 +156,7 @@ function removeExclamationMarks(s) {
   function greet (name, owner) {
     return name == owner ? 'Hello boss' : 'Hello guest';
   }
+
+  function pillars(num_pill, dist, width) {
+    return num_pill <= 1 ? 0 : (num_pill - 1) * dist * 100 + (num_pill - 2) * width;
+  }
